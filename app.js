5bcd8fb481bdac986306a1b2f87907d9d63a91fd
Rafiya Sheikh <fatmab2809@gmail.com>
2020-08-04 15:56:26 +0530

    Files changed

--- app.js
+++ app.js
@@ -2,10 +2,27 @@
     'use strict';
 
     angular.module('FirstAngular', [])
-            .controller('Controller', function($scope){
-                $scope.name = "Fatma";
-                $scope.calc = function(){
-                    return "Calculation";
+            .controller('Controller', function($scope , $filter){
+                // $scope.name = "Fatma";
+                // $scope.calc = function(){
+                //     return "Calculation";
+                // }
+                $scope.name ="";
+                $scope.totalValue = 0;
+
+                $scope.display = function(){
+                    $scope.totalValue = Calculate($scope.name);
+                }
+                $scope.upper = function(){
+                    var upCase = $filter('uppercase');
+                    $scope.name = upCase($scope.name);
                 }
             });
+    function Calculate(string){
+        var stringValue =0;
+        for( var i = 0; i<string.length; i++){
+            stringValue += string.charCodeAt(i)
+        }
+        return stringValue;
+    }
 })();
